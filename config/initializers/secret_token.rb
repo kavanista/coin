7c3dc1d2258bef08cb86e70446ce1f063f417181
Kavanista <john@kavanista.com>
2013-04-13 01:19:12 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Coin::Application.config.secret_token = '68eb353d4297bb10586bc7e5fbe89495d64cac37217714a9340261236f548530e38ea02260e02b6d771cdffdef0162a016433c28ea8bbefa16f7476c43ff741d'
